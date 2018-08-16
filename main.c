553f4bec9a92b41107914e5cc1cad389c4244ade
Spooks01 <S.Fish@student.liverpool.ac.uk>
2018-08-16 20:06:22 +0100

    collision with tail

--- main.c
+++ main.c
@@ -7,20 +7,19 @@ struct bodyPart{
 	int col;
 	struct bodyPart *prev;
 	struct bodyPart *next;
-	char toPrint;
 } typedef bodyPart;
 
 struct food{
-	int row;
-	int col;
+	int y;
+	int x;
 } typedef food;
 
 
-food foo;
+food *foo;
 bodyPart *head, *end, *current;
 int running = 1;
 int input = KEY_RIGHT;
-
+int score = 0;
 void listenForInput(){
 	while(running){
 		int temp;
@@ -28,6 +27,33 @@ void listenForInput(){
 		if (temp == KEY_DOWN || temp == KEY_UP || temp == KEY_LEFT || temp == KEY_RIGHT){
 			input = temp;
 		} 
+	}
+}
+
+void spawnFood(){
+	int width, height;
+	getmaxyx(stdscr, height, width);
+	foo = malloc(sizeof(food));
+	foo->y = rand() % (height - 2);
+	foo->x = rand() % (width - 2);
+	mvaddch(foo->y, foo->x, 'o');
+	while(running){
+		usleep(10000000);
+		mvaddch(foo->y, foo->x, ' ');	
+		foo->y = rand() % (height - 2);
+		foo->x = rand() % (width - 2);
+		mvaddch(foo->y, foo->x, 'o');
+	}
+}
+
+void checkCollision(){
+	bodyPart *currentCheck;
+	currentCheck = head->prev;
+	while(currentCheck != NULL){
+		if (head->row == currentCheck->row && head->col == currentCheck->col){
+			running = 0;
+		}				
+	currentCheck = currentCheck->prev;
 	}
 }
 
@@ -49,24 +75,45 @@ int main() {
 		mvaddch(i, 0, '%');
 	}
 	refresh();
-
 	//create the snake
 	head = malloc(sizeof(bodyPart));
 	head->row = height/2;
 	head->col = width/2;
 	head->next = NULL;
-	head->prev = NULL;
-	head->toPrint = '$';
-	mvaddch(head->row, head->col, head->toPrint);
+	end = malloc(sizeof(bodyPart));
+	end->row = head->row;
+	end->col = head->col - 1;
+	end->next = head;
+	end->prev = NULL;
+	head->prev = end;
+	mvaddch(head->row, head->col, '$');
+	mvaddch(end->row, end->col, '*');
 	refresh();
-	current = head;
 	//input listener thread
-	pthread_t listener;
+	pthread_t listener, foodSpawner, collisionCheck;
 	pthread_create(&listener, NULL, listenForInput, NULL);
+	pthread_create(&foodSpawner, NULL, spawnFood, NULL);
+	//pthread_create(&collisionCheck, NULL, checkCollision, NULL);
 	while(running){
+		if ((foo != NULL) && (head->row == foo->y) && (head->col == foo->x)) {
+			score++;
+			end->prev = malloc(sizeof(bodyPart));
+			end->prev->next = end;
+			end = end->prev;
+			//add bodypart
+		}
 		//head move
 		if (head->row < height - 1 && head->col < width - 1 && head->row > 0 && head->col > 0){
-			mvaddch(head->row, head->col, ' ');
+			//mvaddch(head->row, head->col, ' ');
+			//work back from head, then move head along, then print everything and add blank after end piece
+			current = end;
+			mvaddch(end->row, end->col,' ');
+			while (current->next != NULL){
+				current->row = current->next->row;
+				current->col = current->next->col;
+				mvaddch(current->row, current->col, '*');
+				current = current->next;
+			}
 			switch (input){
 				case KEY_UP:
 					head->row = head->row - 1;
@@ -81,15 +128,21 @@ int main() {
 					head->col = head->col + 1;
 				break;	
 			}
-			mvprintw(0, width - 20, "%d", input);
-			mvaddch(head->row, head->col, head->toPrint);
+			pthread_create(&collisionCheck, NULL, checkCollision, NULL);
+			mvprintw(0, width - 20, "Score: %d", score);
+			mvaddch(head->row, head->col, '$');
+			pthread_join(collisionCheck, NULL);
 		}
 		else {running = 0;}
 		refresh();
-		usleep(20000);
+		usleep(40000);
 	}
-	pthread_join(listener, NULL);
+	clear();
+	mvprintw(height/2 -1 , width/2 - 10, "Score: %d\0", score);
+	mvprintw(height/2, width/2 - 10, "You Lose, CTRL+c exits\0");
 	refresh();
+	pthread_join(listener, NULL);
+	pthread_join(foodSpawner, NULL);
 	endwin();
 	return 0;
 }
